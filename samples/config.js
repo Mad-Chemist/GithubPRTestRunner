28cfb0179101e38666b56cd130922964acfeca24
Chris Deely <chris@webdeely.com>
2017-11-07 11:18:03 -0500

    external config files & prefix message in error handler

---
+++ samples/config.js
@@ -0,0 +1,4 @@
+//Provide the desired target repo path [USER]/[REPO]
+module.exports = {
+    REPO_PATH: 'Mad-Chemist/GithubPRHooks'
+};
---
+++ samples/credentials.js
@@ -0,0 +1,4 @@
+//Provide the user access token for this application from https://github.com/settings/tokens
+module.exports = {
+    ACCESS_TOKEN: 'foo'
+};
